f1f0afc6e7ab174ab3544d884704e05e5edf61c1
Şeyma Topuz <stopuz746@gmail.com>
2025-02-04 20:20:51 +0300

    Business Layer and Logic Rules

---
+++ CSharpEgitimKampi301.BusinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpEgitimKampi301.EntityLayer.Concrete;
+
+namespace CSharpEgitimKampi301.BusinessLayer.Abstract
+{
+    public interface ICategoryService : IGenericService<Category>
+    {
+        void Delete(Category entity);
+    }
+}
---
+++ CSharpEgitimKampi301.BusinessLayer/Concrete/Product Manager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpEgitimKampi301.BusinessLayer.Abstract;
+using CSharpEgitimKampi301.EntityLayer.Concrete;
+
+namespace CSharpEgitimKampi301.BusinessLayer.Concrete
+{
+    public class Product_Manager : IProductService
+    {
+        public void TDelete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> TGetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
